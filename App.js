6516fa8ac553d5bc79fa3815f34df645e3531dac
tamaki <hbk2002320@gmail.com>
2021-01-31 13:29:53 +0900

    feat/news-list-item-component

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
+import ListItem from "./components/ListItem";
+import articles from "./dummies/articles.json";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+
   },
 });
+
+export default function App() {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUri={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item,index) => index.toString()}
+      />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
