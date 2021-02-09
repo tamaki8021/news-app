24a947693c617124530547ac8ffedcec042e2c79
tamaki <hbk2002320@gmail.com>
2021-02-09 16:15:53 +0900

    feat/action-addclip-deleteclip_articlescreen.js

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import {StyleSheet, SafeAreaView, Text} from 'react-native'
+import {StyleSheet, SafeAreaView, Text, TouchableOpacity} from 'react-native'
 import {WebView} from 'react-native-webview'
+import {useDispatch} from 'react-redux'
+import {addClip, deleteClip} from '../store/actions/user'
 
 const styles = StyleSheet.create({
   container: {
@@ -11,9 +13,16 @@ const styles = StyleSheet.create({
 
 export default ArticleScreen = ({route}) => {
   const {article} = route.params
+  const dispatch = useDispatch()
 
   return(
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => {dispatch(addClip({clip: article}))}}>
+        <Text style={{margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {dispatch(deleteClip({clip: article}))}}>
+        <Text style={{margin: 10, fontSize: 30 }}>DELTE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{uri: article.url}} />
     </SafeAreaView>
   )
