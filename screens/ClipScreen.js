7420e8facda8c880c9657aa4ca4d0e9a8c170be8
tamaki <hbk2002320@gmail.com>
2021-02-10 23:28:36 +0900

    feat/clipbutton

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,34 +1,32 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
-import {StyleSheet, SafeAreaView, Text, FlatList} from 'react-native'
-import ListItem from '../components/ListItem'
+import React from "react";
+import { useSelector } from "react-redux";
+import { StyleSheet, SafeAreaView, Text, FlatList } from "react-native";
+import ListItem from "../components/ListItem";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
-  }
-})
+    backgroundColor: "#fff",
+  },
+});
 
-export default ClipScreen = ({navigation}) => {
-  const user = useSelector(state => state.user)
-  const {clips} = user
+export default ClipScreen = ({ navigation }) => {
+  const user = useSelector((state) => state.user);
 
-  return(
+  return (
     <SafeAreaView style={styles.container}>
-      <FlatList data={clips} renderItem={({item}) => (
-        <ListItem
+      <FlatList
+        data={user.clips}
+        renderItem={({ item }) => (
+          <ListItem
             imageUri={item.urlToImage}
             title={item.title}
             author={item.author}
-            onPress={() => navigation.navigate("Article", {article: item})}
-        />
-        )
-      }
-
-      keyExtractor = {(item,index) => index.toString()}
-        
+            onPress={() => navigation.navigate("Article", { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
     </SafeAreaView>
-  )
-}
+  );
+};
